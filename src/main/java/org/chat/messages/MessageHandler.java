8ee36b9cc755858f898ab49a511dfbaa7f408170
Arina Ivanova <arishkamu@gmail.com>
2025-05-24 18:25:53 +0300

    add messageHandler comments

--- src/main/java/org/chat/messages/MessageHandler.java
+++ src/main/java/org/chat/messages/MessageHandler.java
@@ -24,36 +24,45 @@ public class MessageHandler {
     }
 
     public void connect(OnMessageReceived onReceive) throws IOException, TimeoutException {
+        // create factory, channel, connection
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(host);
         connection = factory.newConnection();
         channel = connection.createChannel();
+
+        // set queue parameters
         queueName = channel.queueDeclare().getQueue();
         onReceived = onReceive;
         switchChannel(currentChannel);
     }
 
     public void switchChannel(String newChannel) throws IOException {
+        // check if channel is set. Throw otherwise
         if (channel == null) throw new IllegalStateException("Channel not open");
 
+        // check if queue is already bind
         if (currentChannel != null) {
             channel.queueUnbind(queueName, currentChannel, "");
         }
 
+        // set new channel and queue
         currentChannel = newChannel;
         channel.exchangeDeclare(currentChannel, BuiltinExchangeType.FANOUT, true);
         channel.queueBind(queueName, currentChannel, "");
 
+        // set consumer
         channel.basicConsume(queueName, true, (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             onReceived.onMessage(message);
         }, consumerTag -> {});
     }
 
+    // send message to currentChannel
     public void sendMessage(String message) throws Exception {
         channel.basicPublish(currentChannel, "", null, message.getBytes(StandardCharsets.UTF_8));
     }
 
+    // close channel and connection
     public void close() throws IOException, TimeoutException {
         channel.close();
         connection.close();
